356f3dd0660db9361c5ca90cf69543cacd09f63c
Olivier Mac 2024 <olgil12@ulaval.ca>
2025-05-15 11:57:17 -0400

    save transformed data to numpy array .txt file

--- transform_data.py
+++ transform_data.py
@@ -13,11 +13,10 @@ def load_ASASSN_data(object_name):
     """
     load an ASAS-SN csv lightcurve from the object name
 
-    returns: lc array containing object name + data
+    returns: lc tuple containing object name + data
     """
     #data = np.loadtxt(f"data/raw/{object_name}.csv", skiprows=12)
     data = pd.read_csv(f"data/raw/{object_name}.csv", header=9)
-    print(data.columns)
     jd, flux, flux_error = data["JD"], data["Flux"], data["Flux Error"]
     times = (jd-jd[0])*86400 # Put the start time at 0 and convert to seconds
     exposures = np.ones_like(jd)*270 # For ASAS-SN, each epoch consists of 3x90s exposures.
@@ -27,7 +26,7 @@ def load_ASASSN_data(object_name):
 
 
 def plot_lightcurve(input_lc, units="days"):
-    """Here input_lc is only the object returned by load_data functions"""
+    """Here input_lc is only the tuple returned by load_data functions"""
     times, y, dy, exposures = input_lc[1]
     fig = plt.figure()
     if units == "days":
@@ -41,11 +40,13 @@ def plot_lightcurve(input_lc, units="days"):
 
 def save_lc(lc):
     """
-    
+    save a lc (the tuple returned by load_data functions) as numpy array with name of the object
     """
-    pass
+    object_name, lc_array = lc
+    np.savetxt(f"data/transformed/{object_name}.txt", lc_array.T)
 
 if __name__ == "__main__":
     lc = load_ASASSN_data("mrk421")
     plot_lightcurve(lc, units="seconds")
     plt.show()
+    save_lc(lc)
